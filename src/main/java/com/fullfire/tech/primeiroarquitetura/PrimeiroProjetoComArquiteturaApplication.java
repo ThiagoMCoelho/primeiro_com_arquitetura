c5b1c62542a065850be7ea24bdba04929a9fab91
ThiagoMCoelho <gpcoelho@gmail.com>
2019-03-13 22:50:16 -0300

    Projeto Criado!

---
+++ src/main/java/com/fullfire/tech/primeiroarquitetura/PrimeiroProjetoComArquiteturaApplication.java
@@ -0,0 +1,13 @@
+package com.fullfire.tech.primeiroarquitetura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoComArquiteturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoComArquiteturaApplication.class, args);
+	}
+
+}
