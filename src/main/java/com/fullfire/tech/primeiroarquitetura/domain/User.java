20860958bee389522856a0a305fef3f6212746cc
Thiago Machado Coelho <gpcoelho@gmail.com>
2019-03-14 14:34:09 -0300

    Adicionada classe de dominio

---
+++ src/main/java/com/fullfire/tech/primeiroarquitetura/domain/User.java
@@ -0,0 +1,84 @@
+package com.fullfire.tech.primeiroarquitetura.domain;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String name;
+	private String lastname;
+	private Integer age;
+	
+	public User() {
+		
+	}
+
+	public User(Integer id, String name, String lastname, Integer age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.lastname = lastname;
+		this.age = age;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+
+}
--- src/main/java/com/fullfire/tech/primeiroarquitetura/resources/UserResource.java
+++ src/main/java/com/fullfire/tech/primeiroarquitetura/resources/UserResource.java
@@ -1,16 +1,30 @@
 package com.fullfire.tech.primeiroarquitetura.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fullfire.tech.primeiroarquitetura.domain.User;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST esta funcionando!";
+	public List<User> listar() {
+
+		User user1 = new User(1,"Thiago","Coelho",36);
+		User user2 = new User(2,"Elaine","Coelho",33);
+		
+		List<User> listUser = new ArrayList<>();
+		listUser.add(user1);
+		listUser.add(user2);
+		
+		return listUser;
+		
 	}
 
 }
